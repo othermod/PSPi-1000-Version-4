394645f894c54fa42c9c011b8a4e16ac6b0063eb
otherMod <22604540+othermod@users.noreply.github.com>
2017-11-28 18:43:24 -0500

    Fixed path

--- joystick/install.sh
+++ joystick/install.sh
@@ -15,10 +15,10 @@ grep joystick /etc/rc.local >/dev/null
 if [ $? -eq 0 ]; then
 	echo "joystick already exists in rc.local. Re-creating."
 	# joystick already in rc.local, but make sure correct:
-	sed -i "s/^.*joystick.*$/python \/boot\/examples\/joystick.py \&/g" /etc/rc.local >/dev/null
+	sed -i "s/^.*joystick.*$/python \/boot\/joystick\/joystick.py \&/g" /etc/rc.local >/dev/null
 else
 	echo "joystick doesn't exist in rc.local. Creating."
 	# Insert joystick into rc.local before final 'exit 0'
-	sed -i "s/^exit 0/python \/boot\/examples\/joystick.py \&\\nexit 0/g" /etc/rc.local >/dev/null
+	sed -i "s/^exit 0/python \/boot\/joystick\/joystick.py \&\\nexit 0/g" /etc/rc.local >/dev/null
 fi
 
