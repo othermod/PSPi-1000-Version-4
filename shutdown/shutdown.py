f66a6bd2a5dbeb93d8df5473570eb1fc2fccc00c
otherMod <22604540+othermod@users.noreply.github.com>
2017-12-02 21:46:54 -0500

    description

--- shutdown/shutdown.py
+++ shutdown/shutdown.py
@@ -5,22 +5,21 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-# Use the Broadcom SOC Pin numbers
-# Setup the Pin with Internal pullups enabled and PIN in reading mode.
+# Set up GPIO pin
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 # Our function on what to do when the button is pressed
 def Shutdown(channel):
-	#Dim backlight before shutdown. Pulls BL pin low, allowing backlight to kill as soon as shutdown completes.
+	#Dim backlight before shutdown
+	#Pulls BL pin low, allowing backlight to kill as soon as shutdown completes
 	GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-	#shutddown
+	#Issue shutddown command
 	os.system("sudo shutdown -h now")
 
-# Add our function to execute when the button pressed event happens
+# Interrupt looking for pin voltage to drop
 GPIO.add_event_detect(4, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
 
-# Now wait!
 while 1:
 	time.sleep(1)
 
