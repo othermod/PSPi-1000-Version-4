287c8d5815b901e72dcbcc1a91d7cd88780117b4
otherMod <22604540+othermod@users.noreply.github.com>
2017-12-21 10:13:17 -0500

    added charger detection

--- battery/battery.py
+++ battery/battery.py
@@ -10,7 +10,7 @@ import subprocess
 from subprocess import check_output
 
 status = 0
-
+charging = 0
 #Set debug to 1 to display status
 debug= 0
 
@@ -49,59 +49,85 @@ a = [int(open('/sys/class/hwmon/hwmon0/device/in6_input').read())] * average
 while True:
     	# check battery states
 	a = [int(open('/sys/class/hwmon/hwmon0/device/in6_input').read())] + a[:-1]
+	b = int(open('/sys/class/hwmon/hwmon0/device/in7_input').read())
 	bat = sum(a) / average
 	if debug == 1:
 		print bat
-	if bat < 3630: #change to 0 during troubleshooting
-		changeicon("0")
-		status = 0
-		
-    	elif bat < 3670: #change to 0 during troubleshooting
-        	changeicon("1")
-		
-    	elif bat < 3720:
-		if status != 2:
-			changeicon("2")
-		status = 2
-
-		elif bat < 3740:
-		if status != 3:
-			changeicon("3")
-		status = 3
-
-    	elif bat < 3770:
-		if status != 4:
-			changeicon("4")
-		status = 4
-
-    	elif bat < 3800:
-		if status != 5:
-			changeicon("5")
-		status = 5
-
-    	elif bat < 3870:
-		if status != 6:
-			changeicon("6")
-		status = 6
-		
-    	elif bat < 4000:
-		if status != 7:
-			changeicon("7")
-		status = 7
 	
-    	elif bat < 4040:
-		if status != 8:
-			changeicon("8")
-		status = 8
+	if b > 500:
+		charging = 0
+	if b < 1000 and a[0] > 3800:
+		charging = 1
+	#not charging math
+	#needs to know how to detect charger attached
+	if charging == 0:
+		if bat < 3600: #change to 0 during troubleshooting
+			changeicon("0")
+			status = 0
+		
+    		elif bat < 3638: #change to 0 during troubleshooting
+        		changeicon("1")
+		
+    		elif bat < 3678:
+			if status != 2:
+				changeicon("2")
+			status = 2
+
+		elif bat < 3716:
+			if status != 3:
+				changeicon("3")
+			status = 3
+
+    		elif bat < 3748:
+			if status != 4:
+				changeicon("4")
+			status = 4
+
+    		elif bat < 3786:
+			if status != 5:
+				changeicon("5")
+			status = 5
+
+    		elif bat < 3827:
+			if status != 6:
+				changeicon("6")
+			status = 6
+		
+    		elif bat < 3873:
+			if status != 7:
+				changeicon("7")
+			status = 7
 	
-    	elif bat < 4080:
-		if status != 9:
-			changeicon("9")
-		status = 9
+    		elif bat < 3899:
+			if status != 8:
+				changeicon("8")
+			status = 8
 	
-    	else:
-		if status != 10:
-			changeicon("10")      
-		status = 10
+    		elif bat < 3939:
+			if status != 9:
+				changeicon("9")
+			status = 9
+
+    		else:
+			if status != 10:
+				changeicon("10")      
+			status = 10
+	if charging == 1:
+		if bat < 4023:
+			if status != 11:
+				changeicon("11")
+			status = 11
+		elif bat < 4072:
+			if status != 12:
+				changeicon("12")
+			status = 12
+		elif bat < 4116:
+			if status != 13:
+				changeicon("13")
+			status = 13
+		else:
+			if status != 14:
+				changeicon("14")
+			status = 14
     	time.sleep(refresh)
 
