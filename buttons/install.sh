e35ed1abe6d24cc49af47b31d354de9b0ffcbaad
otherMod <22604540+othermod@users.noreply.github.com>
2017-12-27 12:44:20 -0500

    added I2C autoconfig

--- buttons/install.sh
+++ buttons/install.sh
@@ -94,6 +94,49 @@ else
 	sed -i "s/^exit 0/\/usr\/local\/bin\/retrogame \&\\nexit 0/g" /etc/rc.local >/dev/null
 fi
 
+
+#
+# This script will enable I2C
+#
+ 
+# check if sudo is used
+if [ "$(id -u)" != 0 ]; then
+  echo 'Sorry, you need to run this script with sudo'
+  exit 1
+fi
+ 
+# enable I2C on Raspberry Pi
+echo '>>> Enable I2C'
+if grep -q 'i2c-bcm2708' /etc/modules; then
+  echo 'Seems i2c-bcm2708 module already exists, skip this step.'
+else
+  echo 'i2c-bcm2708' >> /etc/modules
+fi
+if grep -q 'i2c-dev' /etc/modules; then
+  echo 'Seems i2c-dev module already exists, skip this step.'
+else
+  echo 'i2c-dev' >> /etc/modules
+fi
+#if grep -q 'dtparam=i2c1=on' /boot/config.txt; then
+#  echo 'Seems i2c1 parameter already set, skip this step.'
+#else
+#  echo 'dtparam=i2c1=on' >> /boot/config.txt
+#fi
+#if grep -q 'dtparam=i2c_arm=on' /boot/config.txt; then
+#  echo 'Seems i2c_arm parameter already set, skip this step.'
+#else
+#  echo 'dtparam=i2c_arm=on' >> /boot/config.txt
+#fi
+if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
+#  sed -i 's/^blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+  sed -i 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+else
+  echo 'File raspi-blacklist.conf does not exist, skip this step.'
+fi
+
+
+
+
 #grep input_player /opt/retropie/configs/all/retroarch.cfg >/dev/null
 #sed -i "s/^.*input_player1_a =.*$/\input_player1_a = "'"enter"'"/g" /opt/retropie/configs/all/retroarch.cfg >/dev/null
 #sed -i "s/^.*input_player1_b =.*$/\input_player1_b = "'"escape"'"/g" /opt/retropie/configs/all/retroarch.cfg >/dev/null
