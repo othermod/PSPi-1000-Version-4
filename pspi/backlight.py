8c0eb5ca400ab6253cfa046ef3ec429f6aae17d9
otherMod <22604540+othermod@users.noreply.github.com>
2018-02-07 17:51:55 -0500

    new efficient backlight code

--- pspi/backlight.py
+++ pspi/backlight.py
@@ -1,32 +1,32 @@
 import RPi.GPIO as GPIO 
 import time 
 import os  
-duty = 90
+duty = .3
 
 GPIO.setmode(GPIO.BCM)
 #GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setwarnings(False)
 GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-os.system("sudo pigpiod")
+os.system("sudo /boot/pspi/pi-blaster")
 time.sleep(1)
-os.system("sudo pigs p 19 90")
+os.system("echo '19=.3' > /dev/pi-blaster")
 def BL(channel):
+#	print duty
 	global duty
-	duty = duty - 10
-	if duty < 60:
-		duty = 120
-	os.system("sudo pigs p 19 %s" % duty)
+	duty = duty - .1 + .4 * (duty < .3)
+#	print duty
+	os.system("echo '19=%s' > /dev/pi-blaster" % duty)
 
 def Shutdown(channel):
 	# Dim backlight before shutdown
 	# Pulls BL pin low, allowing backlight to kill as soon as shutdown completes
-	os.system("sudo killall pigpiod")
-	GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#	os.system("sudo killall pigpiod")
+#	GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 	# Issue shutddown command
 	os.system("sudo shutdown -h now")
 
-GPIO.add_event_detect(26, GPIO.FALLING, callback = BL, bouncetime = 200)
+GPIO.add_event_detect(26, GPIO.FALLING, callback = BL, bouncetime = 300)
 GPIO.add_event_detect(4, GPIO.FALLING, callback = Shutdown, bouncetime = 200)
 
 
