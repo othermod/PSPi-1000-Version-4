c6ac9ad11fb6af401ae32b0509f58b757e218e89
otherMod <22604540+othermod@users.noreply.github.com>
2018-02-11 19:19:58 -0500

    more gradual brightness changes

--- pspi/backlight.py
+++ pspi/backlight.py
@@ -1,22 +1,24 @@
 import RPi.GPIO as GPIO 
 import time 
 import os  
-duty = .3
-
+duty = [.2, .25, .325, .45]
+bl = 2
 GPIO.setmode(GPIO.BCM)
 #GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setwarnings(False)
 GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-os.system("sudo /boot/pspi/pi-blaster --pcm")
+os.system("sudo /boot/pspi/pi-blasterr --pcm")
 time.sleep(1)
-os.system("echo '19=.3' > /dev/pi-blaster")
+os.system("echo '19=.325' > /dev/pi-blaster")
 def BL(channel):
-#	print duty
 	global duty
-	duty = duty - .1 + .4 * (duty < .3)
-#	print duty
-	os.system("echo '19=%s' > /dev/pi-blaster" % duty)
+	global bl
+	bl = bl - 1
+	if bl < 0:
+		bl = 3
+#	print bl
+	os.system("echo '19=%s' > /dev/pi-blaster" % duty[bl])
 
 def Shutdown(channel):
 	# Dim backlight before shutdown
