89be9137ac9f3372061f894552a46cd42bdbb145
otherMod <22604540+othermod@users.noreply.github.com>
2018-02-12 13:53:57 -0500

    Update backlight.py

--- pspi/backlight.py
+++ pspi/backlight.py
@@ -8,7 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-os.system("sudo /boot/pspi/pi-blasterr --pcm")
+os.system("sudo /boot/pspi/pi-blaster --pcm")
 time.sleep(1)
 os.system("echo '19=.325' > /dev/pi-blaster")
 def BL(channel):
