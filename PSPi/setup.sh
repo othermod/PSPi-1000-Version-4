61399127b517ad7bd92e612a041d3f8a386b685c
othermod <22604540+othermod@users.noreply.github.com>
2021-11-18 11:18:26 -0500

    update config files

--- PSPi/setup.sh
+++ PSPi/setup.sh
@@ -94,7 +94,7 @@ cp -f /boot/PSPi/Configs/pspi_controller.service /etc/systemd/system/pspi_contro
 systemctl enable pspi_controller 2>/dev/null
 
 # fix file permissions
-chmod 0755 /home/pi/PSPi/Driver/pspi-controller
+chmod 0755 /home/pi/PSPi/Driver/pspi
 chmod 0755 /home/pi/PSPi/Driver/pngview
 
 echo "Rebooting"
